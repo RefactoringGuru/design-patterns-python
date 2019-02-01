84c5f4e2176b443e99d0a366b095d47308f01af6
Alexey Pyltsyn <lex61rus@gmail.com>
2019-02-01 03:08:18 +0300

    Visitor: add structural example

---
+++ src/Visitor/Structure/main.py
@@ -0,0 +1,165 @@
+"""
+EN: Visitor Design Pattern
+
+Intent: Represent an operation to be performed over elements of an object
+structure. The Visitor pattern lets you define a new operation without
+changing the classes of the elements on which it operates.
+
+RU: Паттерн Посетитель
+
+Назначение: Позволяет добавлять в программу новые операции, не изменяя классы
+объектов, над которыми эти операции могут выполняться.
+"""
+
+
+from __future__ import annotations
+from abc import ABCMeta, abstractmethod
+from typing import List
+
+
+class Component(metaclass=ABCMeta):
+    """
+    EN: The Component interface declares an `accept` method that should take the
+    base visitor interface as an argument.
+
+    RU: Интерфейс Компонента объявляет метод accept, который в качестве
+    аргумента может получать любой объект, реализующий интерфейс посетителя.
+    """
+
+    @abstractmethod
+    def accept(self, visitor: Visitor) -> None:
+        pass
+
+
+class ConcreteComponentA(Component):
+    """
+    EN: Each Concrete Component must implement the `accept` method in such a way
+    that it calls the visitor's method corresponding to the component's class.
+
+    RU: Каждый Конкретный Компонент должен реализовать метод accept таким
+    образом, чтобы он вызывал метод посетителя, соответствующий классу компонента.
+    """
+
+    def accept(self, visitor: Visitor) -> None:
+        """
+        EN: Note that we're calling `visitConcreteComponentA`, which matches the
+        current class name. This way we let the visitor know the class of the
+        component it works with.
+
+        RU: Обратите внимание, мы вызываем visitConcreteComponentA, что
+        соответствует названию текущего класса. Таким образом мы позволяем
+        посетителю узнать, с каким классом компонента он работает.
+        """
+
+        visitor.visit_concrete_component_a(self)
+
+    def exclusive_method_of_concrete_component_a(self) -> str:
+        """
+        EN: Concrete Components may have special methods that don't exist in
+        their base class or interface. The Visitor is still able to use these
+        methods since it's aware of the component's concrete class.
+
+        RU: Конкретные Компоненты могут иметь особые методы, не объявленные в их
+        базовом классе или интерфейсе. Посетитель всё же может использовать эти
+        методы, поскольку он знает о конкретном классе компонента.
+        """
+
+        return "A"
+
+
+class ConcreteComponentB(Component):
+    """
+    EN: Same here: visitConcreteComponentB => ConcreteComponentB
+
+    RU: То же самое здесь: visitConcreteComponentB => ConcreteComponentB
+    """
+
+    def accept(self, visitor: Visitor):
+        visitor.visit_concrete_component_b(self)
+
+    def special_method_of_concrete_component_b(self) -> str:
+        return "B"
+
+
+class Visitor(metaclass=ABCMeta):
+    """
+    EN: The Visitor Interface declares a set of visiting methods that correspond
+    to component classes. The signature of a visiting method allows the visitor
+    to identify the exact class of the component that it's dealing with.
+
+    RU: Интерфейс Посетителя объявляет набор методов посещения, соответствующих
+    классам компонентов. Сигнатура метода посещения позволяет посетителю
+    определить конкретный класс компонента, с которым он имеет дело.
+    """
+
+    @abstractmethod
+    def visit_concrete_component_a(self, element: ConcreteComponentA) -> None:
+        pass
+
+    @abstractmethod
+    def visit_concrete_component_b(self, element: ConcreteComponentB) -> None:
+        pass
+
+
+"""
+ EN: Concrete Visitors implement several versions of the same algorithm, which
+ can work with all concrete component classes.
+ 
+ You can experience the biggest benefit of the Visitor pattern when using it
+ with a complex object structure, such as a Composite tree. In this case, it
+ might be helpful to store some intermediate state of the algorithm while
+ executing visitor's methods over various objects of the structure.
+ 
+ RU: Конкретные Посетители реализуют несколько версий одного и того же
+ алгоритма, которые могут работать со всеми классами конкретных компонентов.
+ 
+ Максимальную выгоду от паттерна Посетитель вы почувствуете, используя его со
+ сложной структурой объектов, такой как дерево Компоновщика. В этом случае
+ было бы полезно хранить некоторое промежуточное состояние алгоритма при
+ выполнении методов посетителя над различными объектами структуры.
+"""
+
+
+class ConcreteVisitor1(Visitor):
+    def visit_concrete_component_a(self, element) -> None:
+        print(f"{element.exclusive_method_of_concrete_component_a()} + ConcreteVisitor1")
+
+    def visit_concrete_component_b(self, element) -> None:
+        print(f"{element.special_method_of_concrete_component_b()} + ConcreteVisitor1")
+
+
+class ConcreteVisitor2(Visitor):
+    def visit_concrete_component_a(self, element) -> None:
+        print(f"{element.exclusive_method_of_concrete_component_a()} + ConcreteVisitor2")
+
+    def visit_concrete_component_b(self, element) -> None:
+        print(f"{element.special_method_of_concrete_component_b()} + ConcreteVisitor2")
+
+
+def client_code(components: List[Component], visitor: Visitor) -> None:
+    """
+    EN: The client code can run visitor operations over any set of elements
+    without figuring out their concrete classes. The accept operation directs a
+    call to the appropriate operation in the visitor object.
+
+    RU: Клиентский код может выполнять операции посетителя над любым набором
+    элементов, не выясняя их конкретных классов. Операция принятия направляет
+    вызов к соответствующей операции в объекте посетителя.
+    """
+
+    # ...
+    for component in components:
+        component.accept(visitor)
+    # ...
+
+
+if __name__ == "__main__":
+    components = [ConcreteComponentA(), ConcreteComponentB()]
+
+    print("The client code works with all visitors via the base Visitor interface:")
+    visitor1 = ConcreteVisitor1()
+    client_code(components, visitor1)
+
+    print("It allows the same client code to work with different types of visitors:")
+    visitor2 = ConcreteVisitor2()
+    client_code(components, visitor2)
