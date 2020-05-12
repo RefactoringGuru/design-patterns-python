868a3a40d9f487c036ac35552554bb7137ceaa27
Alexander Shvets <neochief@shvetsgroup.com>
2020-05-12 15:52:51 +0300

    Content fixes.

--- src/Prototype/Conceptual/main.py
+++ src/Prototype/Conceptual/main.py
@@ -28,15 +28,18 @@ class SomeComponent:
         `copy.copy` with this object and the returned value is returned as the
         new shallow copy.
         """
+
+        # First, let's create copies of the nested objects.
+        some_list_of_objects = copy.copy(self.some_list_of_objects)
+        some_circular_ref = copy.copy(self.some_circular_ref)
+
+        # Then, let's clone the object itself, using the prepared clones of the
+        # nested objects.
         new = self.__class__(
-            self.some_int, self.some_list_of_objects, self.some_circular_ref
+            self.some_int, some_list_of_objects, some_circular_ref
         )
         new.__dict__.update(self.__dict__)
 
-        # The new object has a new list of objects but with same
-        # objects(shared).
-        new.some_list_of_objects = copy.copy(self.some_list_of_objects)
-        new.some_circular_ref = copy.copy(self.some_circular_ref)
         return new
 
     def __deepcopy__(self, memo={}):
@@ -51,15 +54,18 @@ class SomeComponent:
         to all the `deepcopy` calls you make in the `__deepcopy__` implementation
         to prevent infinite recursions.
         """
+
+        # First, let's create copies of the nested objects.
+        some_list_of_objects = copy.deepcopy(self.some_list_of_objects, memo)
+        some_circular_ref = copy.deepcopy(self.some_circular_ref, memo)
+
+        # Then, let's clone the object itself, using the prepared clones of the
+        # nested objects.
         new = self.__class__(
-            self.some_int, self.some_list_of_objects, self.some_circular_ref
+            self.some_int, some_list_of_objects, some_circular_ref
         )
         new.__dict__.update(self.__dict__)
 
-        # The new object has a new list of objects with different copy of those
-        # objects.
-        new.some_list_of_objects = copy.deepcopy(self.some_list_of_objects, memo)
-        new.some_circular_ref = copy.deepcopy(self.some_circular_ref, memo)
         return new
 
 
@@ -138,11 +144,11 @@ if __name__ == "__main__":
 
     print(
         f"id(deep_copied_component.some_circular_ref.parent): "
-        "{id(deep_copied_component.some_circular_ref.parent)}"
+        f"{id(deep_copied_component.some_circular_ref.parent)}"
     )
     print(
         f"id(deep_copied_component.some_circular_ref.parent.some_circular_ref.parent): "
-        "{id(deep_copied_component.some_circular_ref.parent.some_circular_ref.parent)}"
+        f"{id(deep_copied_component.some_circular_ref.parent.some_circular_ref.parent)}"
     )
     print(
         "^^ This shows that deepcopied objects contain same reference, they "
